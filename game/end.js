dd74f0224c5291aaf4fca09d9c0aa78f1aa81ddb
shaheerzeze <60808656+shaheerzeze@users.noreply.github.com>
2020-10-13 20:22:24 +0530

    commit

--- game/end.js
+++ game/end.js
@@ -1,9 +1,12 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
-
-const highScores = JSON.parse(localStorage.getItem("highScores") || []);
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 10;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -12,4 +15,21 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = (e) => {
     e.preventDefault();
+
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    highScores.push(score);
+
+    highScores.sort( (a, b) => b.score - a.score );
+
+
+
+     highScores.splice(10);
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+
+    window.location.assign("/");
+
 };
