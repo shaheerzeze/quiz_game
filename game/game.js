607a273e8e34d1c7b6d0128ca18acb6c65ac4603
shaheerzeze <60808656+shaheerzeze@users.noreply.github.com>
2020-10-13 08:31:06 +0530

    commit

--- game/game.js
+++ game/game.js
@@ -1,8 +1,8 @@
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -35,11 +35,43 @@ let questions = [
   }
 ];
 
-
-//constants
+//CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
 startGame = () => {
-questionCounter = 0;
-}
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach( choice =>{
+    const number = choice.dataset['number'];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e =>{
+if(!acceptingAnswers) return;
+
+acceptingAnswers = false;
+const selectedChoice = e.target;
+const selectedAnswer = selectedChoice.dataset["number"];
+console.log(selectedAnswer);
+getNewQuestion();
+
+  });
+});
+
+startGame();
