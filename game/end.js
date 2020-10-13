9eb070be019f2d34b3c901468746ed77b5719658
shaheerzeze <60808656+shaheerzeze@users.noreply.github.com>
2020-10-13 18:15:09 +0530

    commit

--- game/end.js
+++ game/end.js
@@ -1,6 +1,8 @@
 const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores") || []);
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 finalScore.innerText = mostRecentScore;
 
