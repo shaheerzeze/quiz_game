b818f726655068f2579a9b62a7d7366eed81cc5a
shaheerzeze <60808656+shaheerzeze@users.noreply.github.com>
2020-10-17 12:35:10 +0530

    commit

--- game/end.js
+++ game/end.js
@@ -5,7 +5,7 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
 
-const MAX_HIGH_SCORES = 10;
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
@@ -27,7 +27,7 @@ saveHighScore = (e) => {
 
 
 
-     highScores.splice(10);
+     highScores.splice(5);
     localStorage.setItem('highScores', JSON.stringify(highScores));
 
     window.location.assign("/");
